6a40b24ec1c73aa4752ec0750955eba02655759a
Mehadi Athani <mehadi.a@fireflink.com>
2023-04-26 10:37:09 +0530

    first

---
+++ src/test/java/com/git/FFGit/com/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.FFGit.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
